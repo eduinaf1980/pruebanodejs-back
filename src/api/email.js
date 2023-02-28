6dfb85b370156d6717eba307d6411972fbea2876
Eduardo Andres funes <eduardo@MacBook-Pro-de-Eduardo.local>
2023-02-28 18:46:00 -0500

    Subir archivos

--- src/api/email.js
+++ src/api/email.js
@@ -1,10 +1,10 @@
 import { Router } from "express";
-import { sendPdf, uploadFile, uploads } from "../controllers/email.js";
+import { sendPdf, uploadFile, uploads, sendFile } from "../controllers/email.js";
 import { Auth } from "../middlewares/auth.js"
 
 const apiEmail = Router();
 
-apiEmail.post('/email', Auth, sendPdf);
+apiEmail.post('/email/addfile', Auth, sendFile);
 apiEmail.post('/file', Auth, uploads)
 
 export default apiEmail;
--- src/controllers/email.js
+++ src/controllers/email.js
@@ -1,22 +1,21 @@
 import fs from "fs";
 import path from "path"
 import nodemailer from "nodemailer"
-import multer from "multer"
-import env from "dotenv"
-env.config()
+import dotenv from 'dotenv';
+dotenv.config();
 
 const pathToAttachment = '/Users/eduardo/Downloads/ticket.pdf';
 
-const storage = multer.diskStorage({
+/*const storage = multer.diskStorage({
     destination: function (req, file, cb) {
         cb(null, 'uploads')
     },
     filename: function (req, file, cb) {
         cb(null, `${Date.now()}-${file.originalname}`)
     }
-})
+})*/
 
-const upload = multer({ storage: storage })
+const upload = ""
 
 export const uploads = (req) => {
     const {myFile} = req.body;
@@ -28,7 +27,15 @@ export const uploadFile = (req, res) => {
     res.send({ data: 'Enviar un archivo' })
 }
 
-export const sendPdf = (req,res)=>{
+export const sendFile = (req, res) => {
+    const {fil, mail} = req.body;
+    if(fs.writeFileSync('src/files/' + fil.name_file, fil.base64, {encoding:'base64'})){
+        console.log("PRUEBA")
+    }
+        //sendPdf(mail,fil.name_file)
+}
+
+export const sendPdf = (mail, name, req, res)=>{
 
     const attachment = fs.readFileSync(pathToAttachment).toString("base64")
 
@@ -38,22 +45,22 @@ export const sendPdf = (req,res)=>{
         port:465,
         secure:true,
         auth:{
-            user: 'eaf1980@gmail.com',
-            pass: 'bvvcllvzoxljblui'
+            user: process.env.GMAIL_USER,
+            pass: process.env.GMAIL_PASSWORD_SMTP,
         },
         tls:{rejectUnauthorized:false}
     })
 
     smtpTransport.sendMail({
-        from: 'ama.eduinaf1980@gmail.com',
-        to: 'eaf1980@gmail.com',
-        subject:'Pdf Generate document',
+        from: 'eaf1980@gmail.com',
+        to: mail,
+        subject:'Envio prueba email pdf',
         html:`
-        Testing Pdf Generate document, Thanks.`,
-        attachments:[
+        Prueba envio mail con pdf.`,
+       attachments:[
             {
-                content:attachment,
-                filename:'ticket.pdf',
+                content:'src/files/'+name,
+                filename: name,
                 contentType: 'application/pdf',
                 path:pathToAttachment
             }
@@ -64,7 +71,7 @@ export const sendPdf = (req,res)=>{
             console.log(error);
         }
         else{
-            res.send("Mail has been sended to your email. Check your mail")
+            res.send({ok: true, message: 'El email ha sido enviado verifique por favor'})
         }
        
     })
