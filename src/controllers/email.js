dad7750710d2a08080ce1fe85e77edecff69f72d
Eduardo Andres funes <eduardo@MacBook-Pro-de-Eduardo.local>
2023-02-28 21:34:10 -0500

    Se sube envio de pdf

--- src/controllers/email.js
+++ src/controllers/email.js
@@ -3,54 +3,50 @@ import nodemailer from "nodemailer"
 import dotenv from 'dotenv';
 dotenv.config();
 
-
 export const sendFile = (req, res) => {
-    const {fil, mail} = req.body;
-    if(fs.writeFileSync('src/files/' + fil.name_file, fil.base64, {encoding:'base64'})){
-        console.log("PRUEBA")
-    }
-    sendPdf(mail,fil.name_file)
-    console.log('Enviado')
-    res.send({ok: true, message: "Se envio con exito el correo"})
+    const { fil, mail } = req.body;
+    var base64Data = req.body.fil.base64.replace(/^data:application\/pdf;base64,/, "");
+    fs.writeFileSync('src/files/' + fil.name_file, base64Data, { encoding: 'base64' })
+    sendPdf(mail, fil.name_file)
+    res.send({ ok: true, message: "Se envio con exito el correo" })
 }
 
-export const sendPdf = (mail, name, req, res)=>{
-
+export const sendPdf = (mail, name, res) => {
 
     let smtpTransport = nodemailer.createTransport({
-        host:'smtp.gmail.com',
-        service:'Gmail',
-        port:465,
-        secure:true,
-        auth:{
+        host: 'smtp.gmail.com',
+        service: 'Gmail',
+        port: 465,
+        secure: true,
+        auth: {
             user: process.env.GMAIL_USER,
             pass: process.env.GMAIL_PASSWORD_SMTP,
         },
-        tls:{rejectUnauthorized:false}
+        tls: { rejectUnauthorized: false }
     })
 
     smtpTransport.sendMail({
         from: 'eaf1980@gmail.com',
         to: mail,
-        subject:'Envio prueba email pdf',
-        html:`
-        Prueba envio mail con pdf.`,
-       attachments:[
+        subject: 'Envio correo de prueba PDF Adjunto',
+        html: `Muy buenas, este es un correo de prueba de nvio de archivos pdf.`,
+        attachments: [
             {
-                content:'src/files/'+name,
+                content: 'src/files/' + name,
                 filename: name,
-                contentType: 'application/pdf'
+                contentType: 'application/pdf',
+                path: 'src/files/' + name
             }
         ]
-    },function(error,info){
-
-        if(error){
+    }, function (error) {
+        if (error) {
             console.log(error);
         }
-        else{
-            res.send({ok: true, message: 'El email ha sido enviado verifique por favor'})
+        else {
+            fs.unlinkSync('src/files/' + name)
+            res.send({ ok: true, message: 'El email ha sido enviado verifique por favor' })
         }
-       
+
     })
 
 
